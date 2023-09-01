55195ddff6b90df99b00e6efa795dd8443cccf19
Puru Gajare <pgajare1@uci.edu>
2023-09-01 16:34:25 -0700

    got the html from the website

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from urllib.request import *
+import ssl
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+URL = "https://www.ics.uci.edu/faculty/"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+response = requests.request(
+    "GET", URL)
+
+html = response.text
+
+
+
+
